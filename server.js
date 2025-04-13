81ade45fd3b427cc3f1338e13e8802bfab37f288
yasser0xD <112259217+yasser0xD@users.noreply.github.com>
2025-04-13 20:52:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware to check "stay logged in" cookie
+
 app.use((req, res, next) => {
     if (!req.session.username && req.cookies['stay-logged-in']) {
         try {
